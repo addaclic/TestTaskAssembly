3a27f022b1ebf858fc97c48efc107d8db5642bf7
addaclic <addaclic@student.21-school.ru>
2025-07-25 22:02:54 +0700

    add: task01 complite

--- task01.py
+++ task01.py
@@ -1,38 +1,52 @@
 import requests
 import time
 from datetime import datetime
-import json
 
 url = f"https://yandex.com/time/sync.json?geo=213"
 
 
 def GetJson(url: str):
-    local_time = time.time()
     try:
-        # local_time = datetime.now()
         response = requests.get(url)
         response.raise_for_status()
         data = response.json()
-        server_time = data['time']/1000
-        print(f'Local  time', local_time)
-        print(f'Server time', server_time)
-        # server_time = datetime.fromtimestamp(data["time"]/1000)
-        timezone_name = data["clocks"]["213"]["name"]
-
-        print(data)
-        print(f"Время сервера и временная зона",
-              datetime.fromtimestamp(server_time), timezone_name)
-
-        delta = local_time - data["time"]/1000
-
-        print(f"Delta time:", delta)
+        return data
 
     except requests.exceptions.RequestException as e:
         print(f"Ошибка при выполнении запроса: {e}")
 
 
+def OutputData(data) -> datetime:
+    print(data)
+    server_timestamp = datetime.fromtimestamp(
+        (data["time"] - data["clocks"]["213"]["offset"]) / 1000
+    )
+    timezone_UTC = data["clocks"]["213"]["offsetString"]
+    timezone_name = data["clocks"]["213"]["name"]
+    readeble_time = server_timestamp.strftime("%d-%m-%Y %H:%M:%S")
+    print(
+        f"\033[38;2;201;100;59mВремя сервера и временная зона\033[0m",
+        readeble_time,
+        timezone_UTC,
+        timezone_name,
+    )
+    return server_timestamp
+
+
 def main():
-    GetJson(url)
+    deltas = []
+    for i in range(5):
+        local_time = datetime.now()
+        jsonfile = GetJson(url)
+        server_time = OutputData(jsonfile)
+        delta_ms = (local_time - server_time).total_seconds()
+        print(f"\033[38;2;201;100;59mDelta time:\033[0m {delta_ms: .2f} s")
+        if delta_ms is not None:
+            deltas.append(delta_ms)
+        time.sleep(1)
+    if deltas:
+        avg_delta = sum(deltas) / len(deltas)
+        print(f"\033[32mСредняя дельта:\033[0m {avg_delta:.2f} мс")
 
 
 if __name__ == "__main__":
